0e446302baebcb0b3aacd8cac570f78855187aba
tconey23 <145934889+tconey23@users.noreply.github.com>
2025-12-02 23:32:59 -0700

    pushing recent updates

--- App.js
+++ App.js
@@ -3,33 +3,10 @@ import 'react-native-url-polyfill/auto';
 import 'react-native-get-random-values';
 import { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
-import {
-  SafeAreaView,
-  SafeAreaProvider,
-  useSafeAreaInsets
-} from 'react-native-safe-area-context';
+import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
 
 import InputForm from './components/InputForm.jsx';
 import AppHeader from './components/AppHeader.js';
-// Supabase import is fine even if unused here
-import { supabase } from './helpers/supabase.js';
-
-const SafeArea = () => {
-  const insets = useSafeAreaInsets()
-
-
-  return (
-    <View style={{
-      width: '100%', 
-      justifyContent: 'flex-end', 
-      marginTop: insets.top,
-      height: '100%'
-      }}>
-          {/* <AppHeader setResetForm={setResetForm}/>
-          <InputForm resetForm={resetForm}/> */} 
-    </View>
-  ) 
-}
 
 export default function App() {
   const [resetForm, setResetForm] = useState(0)
